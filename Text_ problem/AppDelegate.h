d27f1c0626d1ddf11831bd62e8d58507f09fe006
jiboxue <jiboxue@michael.local>
2013-01-01 22:15:02 +0800

    Initial Commit

---
+++ Text_ problem/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Text_ problem
+//
+//  Created by jiboxue on 13-1-1.
+//  Copyright (c) 2013年 jiboxue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Text_ problem/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Text_ problem
+//
+//  Created by jiboxue on 13-1-1.
+//  Copyright (c) 2013年 jiboxue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
